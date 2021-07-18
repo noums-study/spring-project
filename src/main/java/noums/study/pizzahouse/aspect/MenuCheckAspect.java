0494e23ceb567d15ed479bb98527f4bdc932cda9
ssyoni <jsy950601@gmail.com>
2021-07-18 12:58:21 +0900

    메뉴체크 AOP

--- src/main/java/noums/study/pizzahouse/aspect/MenuCheckAspect.java
+++ src/main/java/noums/study/pizzahouse/aspect/MenuCheckAspect.java
@@ -1,5 +1,8 @@
 package noums.study.pizzahouse.aspect;
 
+import net.bytebuddy.implementation.bytecode.Throw;
+import noums.study.pizzahouse.bean.OrderRequest;
+import noums.study.pizzahouse.enums.FoodMenu;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,9 +14,25 @@ public class MenuCheckAspect {
 
     @Around("execution(public * noums.study.pizzahouse.controller.*..*(..))")
     public Object menuCheck(ProceedingJoinPoint joinPoint) throws Throwable {
+
         Object proceed = null;
         System.out.println(">>>> MenuCheckAspect start ...");
-        proceed = joinPoint.proceed();
+
+        try {
+            Object[] args = joinPoint.getArgs();
+            OrderRequest orderRequest = (OrderRequest) args[0];
+            FoodMenu menu = orderRequest.getMenu();
+            System.out.println(menu);
+            if (menu == null) {
+                System.out.println("판매중인 메뉴가 아닙니다.");
+                throw new Exception("판매중인 메뉴가 아닙니다.");
+            }
+            proceed = joinPoint.proceed();
+
+        } catch (Exception e) {
+
+        }
+
         System.out.println(">>>> MenuCheckAspect end ...");
         return proceed;
     }
--- src/main/java/noums/study/pizzahouse/enums/FoodMenu.java
+++ src/main/java/noums/study/pizzahouse/enums/FoodMenu.java
@@ -1,6 +1,16 @@
 package noums.study.pizzahouse.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+
+import javax.swing.text.html.Option;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 public enum FoodMenu {
@@ -13,10 +23,21 @@ public enum FoodMenu {
     CHEESE_PIZZA(8000, "pizza"),
     POTATO_PIZZA(11000, "pizza");
 
-
-
     private int price;
     private String part;
 
     FoodMenu(int price, String part) { this.price = price; this.part = part;}
+
+    @JsonCreator
+    public static FoodMenu fromJson(String menu){
+        System.out.println(">>>>>> JsonCreater :: "+ menu);
+        for (FoodMenu foodMenu : FoodMenu.values()) {
+            if ( foodMenu.toString().equals(menu) ) {
+                System.out.println(">>>>>> correct !");
+                return foodMenu;
+            }
+        }
+        return null;
+    }
+
 }
